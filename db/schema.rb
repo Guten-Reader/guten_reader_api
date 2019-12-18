9d53ccc941fd8d3ab3c2f2fb53003d75ace99e07
fentontaylor <fentontaylor@gmail.com>
2019-12-18 15:33:47 -0700

    Generate Book model and migration
    Co-authored-by: Mack Halliday <16658577+MackHalliday@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_18_221652) do
+ActiveRecord::Schema.define(version: 2019_12_18_222423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.integer "guten_id", null: false
+    t.string "author"
+    t.string "title"
+    t.string "img_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["guten_id"], name: "index_books_on_guten_id", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
