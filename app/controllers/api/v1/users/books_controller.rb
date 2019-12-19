acf4db6002db5808f1a50a597f307006e4872089
MackHalliday <mackenzie.halliday@gmail.com>
2019-12-19 08:15:40 -0700

    Add create method to post to book and user_book when adding book to user's checkedout list

--- app/controllers/api/v1/users/books_controller.rb
+++ app/controllers/api/v1/users/books_controller.rb
@@ -1,7 +1,17 @@
 class Api::V1::Users::BooksController < ApplicationController
   def create
-    facade.validate_params(params)
-    # book = Book.find_or_create_book(book_params)
-    # user_book = UserBook.create_user_book(book.id, user_id)
+    book = Book.where(book_params).first_or_create
+    user_book = UserBook.where(book_id: book.id, user_id: params['user_id']).first_or_create
+    if book && user_book
+      render json: { message: "#{book.title} has been added to user: #{user_book.user_id}"}, status: 201
+    end
+  end
+
+  private
+
+  def book_params
+    { guten_id: params['guten_id'].to_i,
+      title: params['title'],
+      author: params['author'] }
   end
 end
