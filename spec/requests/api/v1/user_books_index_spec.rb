cca782e2124ff36269a0ca7b157d713478cce6fb
fentontaylor <fentontaylor@gmail.com>
2019-12-18 19:35:17 -0700

    Add test for user books returning empty array if none

--- spec/requests/api/v1/user_books_index_spec.rb
+++ spec/requests/api/v1/user_books_index_spec.rb
@@ -1,1 +1,15 @@
 require 'rails_helper'
+
+describe 'GET /api/v1/users/:id/books' do
+  it 'returns an empty array if User has no books' do
+    user = create(:user)
+
+    get "/api/v1/users/#{user.id}/books"
+
+    expect(response).to be_successful
+
+    data = JSON.parse(response.body, symbolize_names: true)
+
+    expect(data).to eq({ books: [] })
+  end
+end
