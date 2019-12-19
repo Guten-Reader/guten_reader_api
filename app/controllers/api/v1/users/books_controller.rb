81f80cb72b1719733b013db9048a825b50695438
fentontaylor <fentontaylor@gmail.com>
2019-12-19 13:24:50 -0700

    Add logic for happy path UPDATE user_book

--- app/controllers/api/v1/users/books_controller.rb
+++ app/controllers/api/v1/users/books_controller.rb
@@ -20,4 +20,22 @@ class Api::V1::Users::BooksController < ApplicationController
       render json: { error: "Could not find record with user_id: #{user_id}, book_id: #{book_id}" }, status: 404
     end
   end
+
+  def update
+    record = find_user_book(user_book_params)
+    if record
+      record.update(current_page: params[:current_page])
+      render json: record
+    end
+  end
+
+  private
+
+  def find_user_book(params)
+    UserBook.find_by(user_id: params[:user_id], book_id: params[:id])
+  end
+
+  def user_book_params
+    params.permit(:user_id, :id)
+  end
 end
