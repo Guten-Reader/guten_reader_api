997da063d2689f770866b6897924aa8888a9f328
fentontaylor <fentontaylor@gmail.com>
2019-12-19 13:24:29 -0700

    Add happy path test for UPDATE user_book

---
+++ spec/requests/api/v1/user_books_update_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'PATCH /api/v1/users/:user_id/books/:book_id' do
+  it 'updates the current page of a user_book' do
+    user, book = create(:user), create(:book)
+    user_book = create(:user_book, user: user, book: book, current_page: 10)
+
+    expect(user_book.current_page).to eq(10)
+
+    patch "/api/v1/users/#{user.id}/books/#{book.id}?current_page=11"
+
+    data = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response.status).to eq(200)
+    expect(data[:user_id]).to eq(user_book.user_id)
+    expect(data[:book_id]).to eq(user_book.book_id)
+    expect(data[:current_page]).to eq(11)
+  end
+end
