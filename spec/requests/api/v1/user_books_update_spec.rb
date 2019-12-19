b297eaf88821a304b68079492763e01c9fc70edd
fentontaylor <fentontaylor@gmail.com>
2019-12-19 14:49:34 -0700

    Add sad path for 404 response in UPDATE

--- spec/requests/api/v1/user_books_update_spec.rb
+++ spec/requests/api/v1/user_books_update_spec.rb
@@ -11,7 +11,7 @@ describe 'PATCH /api/v1/users/:user_id/books/:book_id' do
 
     data = JSON.parse(response.body, symbolize_names: true)
 
-    expect(response.status).to eq(200)
+    expect(response).to have_http_status(200)
     expect(data[:user_id]).to eq(user_book.user_id)
     expect(data[:book_id]).to eq(user_book.book_id)
     expect(data[:current_page]).to eq(11)
@@ -22,7 +22,16 @@ describe 'PATCH /api/v1/users/:user_id/books/:book_id' do
 
     data = JSON.parse(response.body, symbolize_names: true)
 
-    expect(response.status).to eq(400)
+    expect(response).to have_http_status(400)
     expect(data).to eq({ error: "Missing query param current_page" })
+  end
+
+  it 'returns 404 if user_book not found' do
+    patch "/api/v1/users/1/books/1?current_page=21"
+
+    data = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to have_http_status(404)
+    expect(data).to eq({error: "Could not find record with user_id: 1, book_id: 1"})
   end
 end
