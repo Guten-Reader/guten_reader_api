5191a535c719d942b20a066b4551bcb54e23f6b7
MackHalliday <mackenzie.halliday@gmail.com>
2019-12-21 14:55:49 -0700

    Update until loop from .length == 0 to .empty? Should be more efficient

--- app/facades/parser_facade.rb
+++ app/facades/parser_facade.rb
@@ -7,10 +7,10 @@ class ParserFacade
   def parse_data(data)
     clean_data = data[:body].gsub(/(\S)\r\n(\S){1}/, '\1 \2')
     parsed_book = []
-    until clean_data.length == 0
-       text = clean_data.truncate(1000, separator: /\s/, omission:'')
-       parsed_book << text
-       clean_data.sub!(text, '')
+    until clean_data.empty?
+       text_set = clean_data.truncate(1000, separator: /\s/, omission:'')
+       parsed_book << text_set
+       clean_data.sub!(text_set, '')
     end
     parsed_book
   end
