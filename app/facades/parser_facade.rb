c92e422fcaeaf67f21f70354079f9cbc97b9ff5a
MackHalliday <mackenzie.halliday@gmail.com>
2019-12-21 14:38:32 -0700

    Used truncate to parse by roughtly 1000 characters per page. Once truncated, 1000 character set is removed from original data and moved into a parsed_book array. Parsed_book array return with pages by index.

--- app/facades/parser_facade.rb
+++ app/facades/parser_facade.rb
@@ -5,12 +5,12 @@ class ParserFacade
   end
 
   def parse_data(data)
-    clean = data[:body].gsub(/(\S)\r\n(\S){1}/, '\1 \2')
-    chicken = clean
+    clean_data = data[:body].gsub(/(\S)\r\n(\S){1}/, '\1 \2')
     parsed_book = []
-    until chicken == nil
-      binding.pry
-       parsed_book << chicken.truncate(1000, separator: /\s/)
+    until clean_data.length == 0
+       text = clean_data.truncate(1000, separator: /\s/, omission:'')
+       parsed_book << text
+       clean_data.sub!(text, '')
     end
     parsed_book
   end
