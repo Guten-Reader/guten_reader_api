1061441e87f1ad9e4144387da3cd888110dc1586
MackHalliday <mackenzie.halliday@gmail.com>
2019-12-21 12:37:31 -0700

    Rough method for parsing pages not finished

--- app/facades/parser_facade.rb
+++ app/facades/parser_facade.rb
@@ -6,6 +6,12 @@ class ParserFacade
 
   def parse_data(data)
     clean = data[:body].gsub(/(\S)\r\n(\S){1}/, '\1 \2')
-    # book = clean.split.each_slice(300).map{|a|a.join ' '}
+    chicken = clean
+    parsed_book = []
+    until chicken == nil
+      binding.pry
+       parsed_book << chicken.truncate(1000, separator: /\s/)
+    end
+    parsed_book
   end
 end
