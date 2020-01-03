4622fec487fbf51ba97491cd6e1e27dfabe93dc3
fentontaylor <fentontaylor@gmail.com>
2020-01-03 14:43:55 -0700

    Add model test for UserBook
    Co-authored-by: Mack Halliday <16658577+MackHalliday@users.noreply.github.com>

--- app/models/user_book.rb
+++ app/models/user_book.rb
@@ -3,8 +3,7 @@ class UserBook < ApplicationRecord
   belongs_to :book
 
   validates_presence_of :book_id, :user_id
-  validates :book_id, uniqueness: { scope: :user_id}
-
+  validates :book_id, uniqueness: { scope: :user_id }
 
   def self.user_checked_out_book?(book_id, user_id)
     UserBook.find_by(book_id: book_id, user_id: user_id)
