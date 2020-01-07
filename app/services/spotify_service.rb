f52ae14890deb14e857d5bd791032ba0d4a251d8
MackHalliday <mackenzie.halliday@gmail.com>
2020-01-07 10:00:59 -0700

    Update with correct Faraday request, endcode_www_form for request body
    Co-Authored-By: Fenton Taylor <fentontaylor@gmail.com>

--- app/services/spotify_service.rb
+++ app/services/spotify_service.rb
@@ -1,11 +1,11 @@
 class SpotifyService
 
   def get_access_token(user)
-    response = conn.post('/token') do |request|
+    response = conn.post('/api/token') do |request|
       request.headers['Content-Type'] = 'application/x-www-form-urlencoded'
       request.headers['Authorization'] = "#{ENV['SPOTIFY_APP_API_KEY']}"
-      request.body = {grant_type: "refresh_token",
-                      refresh_token: "#{user.refresh_token}"}.to_json
+      request.body = URI.encode_www_form({grant_type: "refresh_token",
+                      refresh_token: "#{user.refresh_token}"})
     end
     parsed_data = JSON.parse(response.body, symbolize_names: true)
     parsed_data[:access_token]
@@ -14,7 +14,7 @@ class SpotifyService
   private
   def conn
     Faraday.new(
-        url: 'https://accounts.spotify.com/api'
+        url: 'https://accounts.spotify.com'
       )
   end
 end
